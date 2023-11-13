aa4905e745f7f5f545756ef998337e7ed2e5d488
bdcoller <bdcoller@gmail.com>
2023-11-13 01:03:32 -0600

    lotsa new work

--- Models/Cart.cs
+++ Models/Cart.cs
@@ -3,30 +3,162 @@ using System;
 
 public partial class Cart : Node3D
 {
-	MeshInstance3D axleBar;
-	BoxMesh axleBarMesh;
+	//Node3D cartFrame;
+	// MeshInstance3D axleBar;
+	// BoxMesh axleBarMesh;
 
+	float wheelRad;        // rear wheel radius
+	float wheelRadS;       // radius of steered wheel
+	float wheelSep;        // separation of rear wheels, center to center
+	float baseLen;         // base length rear axle to steer axis
+	float casterLen;       // caster length for front wheel
+	float tireWd;         // rear tire width
+	float wheelWd;        // rear wheel width
+	float tireWdS;        // steered tire width
 	float barWidth;        // width of bar
-	float axleBarLen;      // length of the axle bar
-	float longBarLen;      // length of the longitudinal bar.
+	// float axleBarLen;      // length of the axle bar
+	// float longBarLen;      // length of the longitudinal bar.
+	float cgDist;          // dist of cg ahead of wheel axle
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GD.Print("Cart");
 
-		// bar dimensions
+		// default parameters
+		wheelRad = 0.5f * 0.75f;
+		wheelRadS = 0.2f;
+		wheelSep = 1.0f;
+		baseLen = 1.3f;
+		casterLen = 0.3f;
+		tireWd = 0.08f;
+		wheelWd = 0.5f * tireWd;
+		tireWdS = 0.2f * (float)wheelRadS;
 		barWidth = 0.08f;
-		axleBarLen = 1.0f;
-		longBarLen = 1.3f;
+		cgDist = 0.6f;
 
-		axleBar = GetNode<MeshInstance3D>("CartFrame/AxleBar");
-		axleBarMesh = (BoxMesh)axleBar.Mesh;
-		axleBarMesh.Size = new Vector3(barWidth, barWidth, axleBarLen);
+		// axleBarLen = 1.0f;
+		// longBarLen = 1.3f;
+
+		//axleBar = GetNode<MeshInstance3D>("CartFrame/AxleBar");
+		//cartFrame = GetNode<Node3D>("CartFrame");
+		BuildModel();
+		// axleBarMesh = (BoxMesh)axleBar.Mesh;
+		// axleBarMesh.Size = new Vector3(barWidth, barWidth, axleBarLen);
 	}
+
+	
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		
+		
+	}
+
+	//------------------------------------------------------------------------
+	// BuildModel:
+	//------------------------------------------------------------------------
+	public void BuildModel()
+	{
+		// axle bar
+		MeshInstance3D axleBar = GetNode<MeshInstance3D>("CartFrame/AxleBar");
+		BoxMesh axleBarMesh = (BoxMesh)axleBar.Mesh;
+		float axleBarLen = wheelSep - 1.5f*wheelWd;
+		axleBarMesh.Size = new Vector3(barWidth, barWidth, axleBarLen);
+
+		// longitudinal bar
+		MeshInstance3D longBar = GetNode<MeshInstance3D>("CartFrame/LongBar");
+		BoxMesh longBarMesh = (BoxMesh)longBar.Mesh;
+		longBarMesh.Size = new Vector3(baseLen+barWidth, barWidth, barWidth);
+		float shft = 0.5f*(baseLen+barWidth) + 0.5f*barWidth;
+		longBar.Position = new Vector3(shft, 0.0f, 0.0f);
+
+		// left wheel
+		MeshInstance3D tireL = 
+			GetNode<MeshInstance3D>("CartFrame/WheelFrameL/Tire");
+		TorusMesh tireMeshL = (TorusMesh)tireL.Mesh;
+		tireMeshL.OuterRadius = wheelRad;
+		tireMeshL.InnerRadius = wheelRad - tireWd;
+
+		// Shift cart frame
+		Node3D cartFrame = GetNode<Node3D>("CartFrame");
+		cartFrame.Position = new Vector3(-cgDist, wheelRad, 0.0f);
+	}
+
+	//------------------------------------------------------------------------
+	// Setters:
+	//------------------------------------------------------------------------
+
+	public float WheelRadius
+	{
+		set
+		{
+			if(value > 0.0f)
+				wheelRad = value;
+		}
+	}
+
+	public float SteeredWheelRadius
+	{
+		set{
+			if(value > 0.0f)
+				wheelRadS = value;
+		}
+	}
+
+	public float WheelSeparation
+	{
+		set{
+			if(value > 0.1f)
+				wheelSep = value;
+		}
+	}
+
+	public float BaseLength
+	{
+		set{
+			if(value > 0.1f)
+				baseLen = value;
+		}
+	}
+
+	public float CasterLength
+	{
+		set{
+			if(value > 0.0f)
+				casterLen = value;
+		}
+	}
+
+	public float TireWidth
+	{
+		set{
+			if(value > 0.0f)
+				tireWd = value;
+		}
+	}
+
+	public float WheelWidth
+	{
+		set{
+			if(value > 0.0f)
+				wheelWd = value;
+		}
+	}
+
+	public float SteeredTireWidth
+	{
+		set{
+			if(value > 0.0f)
+				tireWdS = value;
+		}
+	}
+
+	public float FrameBarWidth
+	{
+		set{
+			if(value > 0.0f)
+				barWidth = value;
+		}
 	}
 }
--- RacerScene.cs
+++ RacerScene.cs
@@ -9,6 +9,18 @@ public partial class RacerScene : Node3D
 	float camDist;
 	Vector3 camTg;       // coords of camera target
 
+	Cart cart;           // model of the cart
+	double wheelRad;     // wheel radius
+	double wheelRadS;    // wheel radius of steared wheel
+	double wheelSep;     // distance separating the two wheels.
+	double baseLen;      // base length, from rear wheel axle to steer axis.
+	double casterLen;    // length of caster
+	double cgDist;       // longit distance from wheel axle to CG
+	float tireThk;       // tire thickness
+	float wheelThk;      // wheel thickness
+	float tireThkS;      // thickness of steered wheel.
+	float frameThk;      // frame thickness
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -24,6 +36,17 @@ public partial class RacerScene : Node3D
 		cam.Distance = camDist;
 		cam.Target = camTg;
 		//cam.FOVDeg = camFOV;
+
+		cart = GetNode<Cart>("Cart");
+		wheelRad = 0.75;
+		wheelRadS = 0.2;
+		wheelSep = 1.0;
+		baseLen = 1.3;
+		casterLen = 0.3;
+		cgDist = 0.6;
+		tireThk = 0.08f;
+		wheelThk = 0.5f * tireThk;
+		tireThkS = 0.2f * (float)wheelRadS;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
